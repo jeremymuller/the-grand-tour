31f724590c21aedbdcdb0a7f6b34016207816034
mullerjs <jeremysmuller@gmail.com>
2017-02-09 08:59:29 -0700

    cleaned up code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-var osc, env, reverb, octave;
-var octaves = [0, 12, 24];
-var mode = [60, 62, 64, 65, 67, 69, 71, 72];
-var delay, count;
 var backgroundHue;
 var startButton, wrapper;
 var play = false;
@@ -9,9 +5,8 @@ var textures;
 var table;
 
 var globalCount = 0;
-var fr = 60; // framerate
-var introDuration, texture1Duration, texture2Duration, texture3Duration, texture4Duration; // in seconds
-var codaDuration;
+var introDuration, texture1Duration, texture2Duration,
+	texture3Duration, texture4Duration, codaDuration;
 
 function preload() {
 	table = loadTable("data/satellites.csv", "csv", "header");
@@ -21,41 +16,29 @@ function setup() {
 	// createCanvas(10, 10);
 
 	colorMode(HSB, 360, 100, 100);
-
 	backgroundHue = int(random(360));
-
-	wrapper = createDiv('');
-	wrapper.class("wrapper");
-	wrapper.id("container");
-
-	startButton = createButton('Tap to start');
-	startButton.class("splash");
-	startButton.parent("container");
-	startButton.mouseReleased(buttonAction);
 
 	var c = color(backgroundHue, 100, 100);
 	var body = select('body');
 	body.style("background-color", color(backgroundHue, 100, 100));
 
+	// start button
+	wrapper = createDiv('');
+	wrapper.class("wrapper");
+	wrapper.id("container");
+	startButton = createButton('Tap to start');
+	startButton.class("splash");
+	startButton.parent("container");
+	startButton.mouseReleased(buttonAction);
+
+	// satellite data
 	var jupiter = table.getColumn("Jupiter pitch");
 	var saturn = subset(table.getColumn("Saturn pitch"), 0, 62);
 	var uranus = subset(table.getColumn("Uranus pitch"), 0, 27);
 	var neptune = subset(table.getColumn("Neptune pitch"), 0, 14);
 	textures = new Textures(jupiter, saturn, uranus, neptune);
 
-	delay = random(1000, 5000); // in milliseconds
-
-	// introDuration = 60 * fr;
-	// texture1Duration = 60 * fr + introDuration;
-
-	// durations for each movement & section
-	// introDuration = random(60, 90) * fr;
-	// texture1Duration = random(150, 180)*fr + introDuration;
-	// texture2Duration = random(145, 155) * fr + texture1Duration;
-	// texture3Duration = random(115, 125) * fr + texture2Duration;
-	// texture4Duration = random(175, 185) * fr + texture3Duration;
-	// codaDuration = 60 * fr + texture4Duration;
-
+	// form of the piece
 	introDuration = random(1, 1.5)*60000;
 	texture1Duration = random(2.5, 3)*60000 + introDuration;
 	texture2Duration = random(2.45, 2.55)*60000 + texture1Duration;
@@ -77,32 +60,14 @@ function setup() {
 	console.log("3: " + texture3Duration);
 	console.log("4: " + texture4Duration);
 	console.log("coda: " + codaDuration);
-
-	env = new p5.Env();
-	// env.setADSR(0.1, 0.2, 0.0, 1);
-	env.setADSR(0.02, 0.0, 1.0, 1);
-	env.setRange(0.9, 0);
-	env.setExp(true);
-
-	count = millis();
-	osc = new p5.Oscillator();
-	// osc.setType('triangle');
-	osc.setType('square');
-
-	octave = random(octaves);
-
-	osc.freq(midiToFreq(random(mode) + octave));
-	osc.amp(env);
-	osc.start();
-
-	// reverb? on a phone??
-	// reverb = new p5.Reverb();
-	// reverb.process(osc, 3, 2);
+	var total = codaDuration/60000
+	console.log("Total length of this instance: " + total.toFixed(2) + "(seconds)");
 }
 
 function draw() {
-	backgroundHue %= 360;
 
+	// animate background color change
+	backgroundHue %= 360;
 	var c = color(backgroundHue, 100, 100);
 	var body = select('body');
 	body.style("background-color", c);
@@ -116,8 +81,6 @@ function draw() {
 		else if ((texture4Duration + globalCount) > millis()) textures.playTexture4();
 		else if ((codaDuration + globalCount) > millis()) textures.playCoda();
 		else theEnd();
-
-		// else if (texture4Duration > globalCount) textures.playTexture4();
 
 		backgroundHue += 0.1;
 		globalCount++;
